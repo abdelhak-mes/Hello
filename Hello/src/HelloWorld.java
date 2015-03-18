debe49b52bdc478455d113021111af9f04dd83f7
abdelhak-mes <abdelhak.mesbah@gmail.com>
2015-03-18 22:09:10 +0100

    The first commit

---
+++ Hello/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("version 1");
+		System.out.println("version 2");
+		System.out.println("version 3");
+		System.out.println("");
+	}
+
+}
